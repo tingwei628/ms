bc40a4a79179af3c3b500a8c6080132ce1f6371c
tingwei628 <sbvq6p199xe@gmail.com>
2024-12-30 18:17:23 +0800

    build: init github actions build image

--- ms/Program.cs
+++ ms/Program.cs
@@ -2,25 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ms
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+// config/secret for different env 
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    // Look for static files in webroot
+    //WebRootPath = "webroot"
+});
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//// Add a custom scoped service.
+//builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
+
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+
+// CORS
+// Authentication
+// Authorization
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapHealthChecks("/health");
+
+app.Run();
